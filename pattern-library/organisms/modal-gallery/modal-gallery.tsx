5e966f3aa5410e034b2a3b85ead2550f123e3429
Juanjo Fernández <juanjo.fernandez@haa.st>
2023-05-17 12:11:09 +0200

    Complete modal gallery light style

--- pattern-library/organisms/modal-gallery/modal-gallery.tsx
+++ pattern-library/organisms/modal-gallery/modal-gallery.tsx
@@ -3,41 +3,13 @@
 import React from "react"
 import Lightbox, { SlideImage } from "yet-another-react-lightbox"
 import { ImageSlide } from "../../atoms/image-slide"
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid"
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  XMarkIcon
+} from "@heroicons/react/24/solid"
 import { ModalGalleryProperties } from "./types"
-
-// shimmer project example: https://github.com/vercel/next.js/blob/canary/examples/image-component/pages/shimmer.tsx
-// const shimmerDark = (w: number, h: number) => `
-// <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-//   <defs>
-//     <linearGradient id="g">
-//       <stop stop-color="#333" offset="20%" />
-//       <stop stop-color="#222" offset="50%" />
-//       <stop stop-color="#333" offset="70%" />
-//     </linearGradient>
-//   </defs>
-//   <rect width="${w}" height="${h}" fill="#333" />
-//   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-//   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-// </svg>`
-const shimmerLight = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#ccc" offset="20%" />
-      <stop stop-color="#ddd" offset="50%" />
-      <stop stop-color="#ccc" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#ccc" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str)
+import { createBlurData } from "@/library/utilities"
 
 export default function ModalGallery({
   index,
@@ -56,6 +28,7 @@ export default function ModalGallery({
   return (
     <Lightbox
       index={index}
+      className="modal"
       // open={index >= 0}
       open={open}
       // close={() => setIndex(-1)}
@@ -65,18 +38,22 @@ export default function ModalGallery({
         (photo) =>
           ({
             src: photo.urls.regular,
-            blurDataURL: `data:image/svg+xml;base64,${toBase64(
-              shimmerLight(photo.width, photo.height)
-            )}`,
+            blurDataURL: createBlurData(photo.width, photo.height),
             width: photo.width,
             height: photo.height
           } as SlideImage)
       )}
       render={{
         slide: ImageSlide,
-        // iconPrev: () => <ChevronLeftIcon className="h-6 w-6 text-blue-500" />
-        iconPrev: () => <ChevronLeftIcon />,
-        iconNext: () => <ChevronRightIcon />
+        // iconPrev: () => <ChevronLeftIcon />,
+        // iconNext: () => <ChevronRightIcon />
+        iconPrev: () => (
+          <ChevronLeftIcon className="h-6 w-6 -translate-x-0.5" />
+        ),
+        iconNext: () => (
+          <ChevronRightIcon className="h-6 w-6 translate-x-0.5" />
+        ),
+        iconClose: () => <XMarkIcon className="h-6 w-6" />
       }}
       on={{
         view: ({ index }) => {
